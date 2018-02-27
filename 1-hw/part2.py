5824375b774a0b0a92b0f014dcf7f29dd3dbf0cc
howinator <howie.benefiel@unizin.org>
2018-02-26 22:52:40 -0600

    Add start of hw2

--- 1-hw/part2.py
+++ 1-hw/part2.py
@@ -14,8 +14,8 @@ def problem1():
     linear_expression = LinExpr()
     for row_idx in range(n):
         for col_idx in range(n):
-            linear_expression.addTerms(cost_matrix[row_idx, col_idx], choice_tuple[row_idx, col_idx])
-
+            linear_expression.addTerms(cost_matrix[row_idx, col_idx],
+                                       choice_tuple[row_idx, col_idx])
 
     m.setObjective(linear_expression, GRB.MINIMIZE)
     m.addConstrs(sum(choice_tuple[worker, i] for i in range(n)) == 1 for worker in range(n))
@@ -23,6 +23,26 @@ def problem1():
 
     m.optimize()
 
+    _print_results(m)
+
+
+def problem2():
+
+    array = [4, 7, 2, 8, 1, 5]
+    r = 3
+    n = len(array)
+    m = Model("mip1")
+
+    choice_tuple = m.addVars(n, 1, vtype=GRB.BINARY, name="choice")
+
+    linear_expression = LinExpr()
+    for row in range(n):
+        linear_expression.addTerms(array[row], choice_tuple[row, 0])
+
+    m.setObjective(linear_expression, GRB.MAXIMIZE)
+    m.addConstr(sum(choice_tuple[row, 0] for row in range(n)) == r)
+
+    m.optimize()
     _print_results(m)
 
 
@@ -41,3 +61,5 @@ def _print_results(m):
 
 print("-----------problem1 below--------------")
 problem1()
+print("-----------problem2 below--------------")
+problem2()
